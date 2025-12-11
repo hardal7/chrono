d39c501441224ad821561c810340f53bb7452b56
hardal <hardal7@protonmail.com>
2025-12-11 18:43:39 +0300

    Implement session creation

--- internal/handler/session/create.go
+++ internal/handler/session/create.go
@@ -4,10 +4,10 @@ import (
 	"net/http"
 	"time"
 
-	logger "github.com/hardal7/study/internal/util"
+	logger "github.com/hardal7/chrono/internal/util"
 
-	"github.com/hardal7/study/internal/model"
-	"github.com/hardal7/study/internal/repository"
+	"github.com/hardal7/chrono/internal/model"
+	"github.com/hardal7/chrono/internal/repository"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -27,14 +27,16 @@ func Create(w http.ResponseWriter, r *http.Request, csr model.CreateSessionReque
 	}
 
 	session := model.Session{
-		Name:      csr.Name,
-		Password:  string(passwordHash),
+		Name:     csr.Name,
+		Password: string(passwordHash),
+		// TODO: Use auth middleware beforehand to get userid
+		// Admin: ,
 		Expiry:    time.Now().Add(time.Minute * time.Duration(csr.Expiry)),
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
 
-	isDuplicate, err := repository.IsDuplicateSession(r.Context(), session)
+	isDuplicate, err := repository.IsDuplicate(r.Context(), session, "sessions")
 	if err != nil {
 		logger.Info("Failed to check if session " + csr.Name + " is duplicate")
 		logger.Debug(err.Error())
@@ -45,7 +47,7 @@ func Create(w http.ResponseWriter, r *http.Request, csr model.CreateSessionReque
 		http.Error(w, "Session with name "+session.Name+" is already created", http.StatusBadRequest)
 		return
 	} else {
-		if err := repository.CreateSession(r.Context(), session); err != nil {
+		if err := repository.Create(r.Context(), session, "sessions"); err != nil {
 			logger.Info("Failed to create session: " + csr.Name)
 			logger.Debug(err.Error())
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
