da0d0b02a583dd1921c29e2ccce0796f6e3ee4de
hardal <hardal7@protonmail.com>
2026-01-09 22:07:59 +0300

    feat: Implement status code and Loki

--- internal/middleware/log.go
+++ internal/middleware/log.go
@@ -2,15 +2,56 @@ package middleware
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	logger "github.com/hardal7/chrono/internal/util"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 func LogRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		next.ServeHTTP(w, r)
-		logger.Debug(r.Method + " " + r.URL.Path + " " + time.Since(start).String())
+		ww := &statusWriter{ResponseWriter: w, status: 200}
+		next.ServeHTTP(ww, r)
+
+		duration := time.Since(start)
+		method := r.Method
+		endpoint := r.URL.Path
+		status := ww.status
+
+		logger.Debug(strconv.Itoa(status) + " " + method + " " + endpoint + " " + duration.String())
+		httpRequestsTotal.WithLabelValues(method, endpoint, http.StatusText(status)).Inc()
+		httpRequestDuration.WithLabelValues(method, endpoint).Observe(float64(duration.Milliseconds()))
 	})
 }
+
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *statusWriter) WriteHeader(status int) {
+	w.ResponseWriter.WriteHeader(status)
+	w.status = status
+}
+
+var (
+	httpRequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "api_requests_total",
+			Help: "Total number of API requests",
+		},
+		[]string{"method", "endpoint", "status"},
+	)
+
+	httpRequestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "api_request_duration_milliseconds",
+			Help:    "API request duration in milliseconds",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "endpoint"},
+	)
+)
